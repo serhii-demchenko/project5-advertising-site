0a73f4e792736d9625cdc7a68547c13041ff887c
Serhii Demchenko <dem.s.vl@gmail.com>
2020-12-10 16:29:38 +0200

    fix request Ads

--- src/js/helpers/API/index.js
+++ src/js/helpers/API/index.js
@@ -100,18 +100,20 @@ export const requestAds = async () => {
     method: 'GET',
     redirect: 'follow',
   };
-  const ads = {};
+  const ads = [];
   for (let i = 1; i <= 3; i += 1) {
     const response = await fetch(
       `https://callboard-backend.herokuapp.com/call?page=${i}`,
       requestOptions,
     );
     const data = await response.json();
+    const obj = {};
     for (const category in data) {
       if (data.hasOwnProperty(category)) {
-        ads[category] = [...data[category]];
+        obj[category] = [...data[category]];
       }
     }
+    ads.push(obj);
   }
   return ads;
 };
