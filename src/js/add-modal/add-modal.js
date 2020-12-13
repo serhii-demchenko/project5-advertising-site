47a2251e6a7a4ea1ed91b99c00ab1684c8b83f05
Helka <grc.helka@gmail.com>
2020-12-13 21:36:29 +0200

    improved markup; added function for images and categories

--- src/js/add-modal/add-modal.js
+++ src/js/add-modal/add-modal.js
@@ -6,3 +6,26 @@ function preview_image(event) {
   };
   reader.readAsDataURL(event.target.files[0]);
 }
+
+const refs = {
+  categorySelect: document.querySelector("#category-select"),
+  categoryList: document.querySelector(".category-list"),
+  optionSelected: document.querySelector(".category-option"),
+  optionName: document.querySelectorAll(".option-name"),
+};
+
+refs.categorySelect.addEventListener("click", onClick);
+
+function onClick(event) {
+  if (refs.categoryList.style.display == "none") {
+    refs.categoryList.style.display = "block";
+  } else {
+    refs.categoryList.style.display = "none";
+  }
+}
+
+refs.optionSelected.addEventListener("click", onSelect);
+
+function onSelect(event) {
+  refs.optionName.textContent = refs.optionSelected.textContent;
+}
