d3e5f9a28362bf43162f41a8cfaaddb216a5b46b
Dmitry Medychenko <rAv3ndima91@gmail.com>
2020-12-14 23:40:41 +0200

    Делает модалку логаута и очистку токенов пользователя

--- src/js/account/account.js
+++ src/js/account/account.js
@@ -1,13 +1,16 @@
 import { openModal, closeModal } from '../modal-window/index';
 import { requestUserLogout } from '../helpers/API/index';
+import { showRegisterBtn } from '../header/header';
+import logoutModalTpl from '../../templates/logoutModalTpl.hbs';
 
+//Часть Элоны (слушатель на кнопке выхода)
 const logoutMainBtnRef = document.querySelector('[data-logout-button]');
 logoutMainBtnRef.addEventListener('click', onMainLogoutBtnClick);
 
+//Функция вызова модалки для выхода
+
 function onMainLogoutBtnClick() {
-  openModal(
-    '<div class="modal-wrapper"><h3 class="modal-title">Ви дійсно хочете вийти з акаунта?</h3><div class="buttons-wrapper"><button type="button" class="modal-logout" >ВИЙТИ</button><button type="button" class="modal-cancel" >ВІДМІНА</button></div></div>',
-  );
+  openModal(logoutModalTpl());
   const modalWrapperRef = document.querySelector('.modal-wrapper');
   modalWrapperRef.parentElement.classList.add('logout-window');
   const logoutModalBtnRef = document.querySelector('.modal-logout');
@@ -17,11 +20,15 @@ function onMainLogoutBtnClick() {
   closeModalBtnRef.addEventListener('click', closeModal, { once: true });
 }
 
+//Функция логаута + очистка токенов пользователя при нажатии на кнопку "ВИЙТИ"
+
 function onModalLogoutBtnClick() {
   requestUserLogout();
   sessionStorage.removeItem('accessToken');
   sessionStorage.removeItem('refreshToken');
   sessionStorage.removeItem('sid');
+  showRegisterBtn();
+  closeModal();
 }
 
 // Здесь нужно добавить импорт ф-ции которая рендерит мои товары
