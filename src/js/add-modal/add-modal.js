acbdf380bd593eb5d58603a10a5ce54b9c163cea
Helka <grc.helka@gmail.com>
2020-12-15 00:25:21 +0200

    added category selection from backend response, rended list of categories

--- src/js/add-modal/add-modal.js
+++ src/js/add-modal/add-modal.js
@@ -2,38 +2,48 @@ import addCallModalTpl from "../../templates/add-modal.hbs";
 import { requestCategories } from "../helpers";
 import { closeModal, openModal } from "../modal-window";
 
-const refs = {
+/*const refs = {
   categorySelect: document.querySelector("#category-select"),
-  categoryList: document.querySelector(".category-select-options > ul"),
-  optionName: document.querySelector(".category-selected-option-name"),
 };
+categoryList: document.querySelector(".category-select-options > ul"),
+  optionName: document.querySelector(".category-selected-option-name"),
+};*/
+
 export function openAddCallModal() {
   openModal(addCallModalTpl());
   document
     .querySelector(".modal-window__item")
     .classList.add("modal-window__add-modal");
-  requestCategories(response).then(renderCategoryOptions).catch(error);
-  refs.categorySelect.addEventListener("click", onCategorySelectClick);
+  requestCategories().then(renderCategoryOptions);
+  document
+    .querySelector("#category-select")
+    .addEventListener("click", onCategorySelectClick);
 }
 
 function renderCategoryOptions(response) {
-  if (response.ok) {
-    refs.categoryList.innerHTML += "`<li>${}</li>`";
-  }
+  response.forEach((category) => {
+    const newLi = document.createElement("li");
+    const newContent = document.createTextNode(category);
+    newLi.appendChild(newContent);
+    newLi.addEventListener("click", onCategoryOptionClick);
+    document.querySelector(".category-select-options > ul").appendChild(newLi);
+  });
 }
+
 function onCategorySelectClick(event) {
-  refs.categoryList.classList.toggle("hidden");
+  document.querySelector(".category-select-options").classList.toggle("hidden");
 }
 
 function onCategoryOptionClick(event) {
-  refs.optionName.textContent = event.textContent;
+  document.querySelector(".category-selected-option-name").textContent =
+    event.target.textContent;
 }
 
-function preview_image(event) {
+/*function preview_image(event) {
   event.target.previousElementSibling.style.display = "none";
   var reader = new FileReader();
   reader.onload = function () {
     event.target.nextElementSibling.src = reader.result;
   };
   reader.readAsDataURL(event.target.files[0]);
-}
+}*/
