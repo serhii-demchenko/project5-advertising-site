f6ba91f88bc32b2d824604a9c65eb76a5084fab1
Serhii Demchenko <dem.s.vl@gmail.com>
2020-12-16 08:15:22 +0200

    fix

--- src/js/product-modal/product-modal.js
+++ src/js/product-modal/product-modal.js
@@ -1,19 +1,33 @@
-const productModalBtnRef = document.querySelector('.js-product-modal__btn--less-on-click');
-const productModalOnClickBtnRef = document.querySelector('.js-product-modal__btn--on-click');
-productModalBtnRef.addEventListener('click', addClassVisuallyHidden);
-productModalOnClickBtnRef.addEventListener('click', removeClassVisuallyHidden);
+// const productModalBtnRef = document.querySelector('.js-product-modal__btn--less-on-click');
+// const productModalOnClickBtnRef = document.querySelector('.js-product-modal__btn--on-click');
+// productModalBtnRef.addEventListener('click', addClassVisuallyHidden);
+// productModalOnClickBtnRef.addEventListener('click', removeClassVisuallyHidden);
 
-export function addClassVisuallyHidden(e) {
-    if (!e.target.classList.contains('visually-hidden')) {
-        e.target.classList.add('visually-hidden')
-    }
+export function productModalAddEventListeners() {
+  document.querySelector('.product-modal').addEventListener('click', e => {
+    e.preventDefault();
+  });
+  const productModalBtnRef = document.querySelector(
+    '.js-product-modal__btn--less-on-click',
+  );
+  const productModalOnClickBtnRef = document.querySelector(
+    '.js-product-modal__btn--on-click',
+  );
+  productModalBtnRef.addEventListener('click', addClassVisuallyHidden);
+  productModalOnClickBtnRef.addEventListener(
+    'click',
+    removeClassVisuallyHidden,
+  );
 }
 
-export function removeClassVisuallyHidden(e) {
+function addClassVisuallyHidden(e) {
+  if (!e.target.classList.contains('visually-hidden')) {
+    e.target.classList.add('visually-hidden');
+  }
+}
+
+function removeClassVisuallyHidden(e) {
   if (e.target.classList.contains('visually-hidden')) {
-      e.target.classList.remove('visually-hidden')
-     }    
+    e.target.classList.remove('visually-hidden');
+  }
 }
-
-
-
