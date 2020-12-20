004a13fc199deccd6658b720a6e129809333af97
Alena <alena.senkiv@gmail.com>
2020-12-20 16:01:53 +0200

    Update remove-my-calls.js

--- src/js/my-calls/remove-my-calls.js
+++ src/js/my-calls/remove-my-calls.js
@@ -4,25 +4,30 @@ import { getUserToken } from '../helpers/index';
 
 // Delete my-calls item
 
-// async function delCallItem(e) {
-//   if (!e.target.classList.contains('селектор класса/айдишника кнопки для удаления')) {
-//     return;
-//   }
-//   const cardId = getId(e);
-//   const userToken = getUserToken();
+export async function delCallItem(e) {
+  if (
+    !e.target.classList
+      .contains
+      // 'селектор класса/айдишника кнопки для удаления',
+      ()
+  ) {
+    return;
+  }
+  const cardId = getId(e);
+  const userToken = getUserToken();
 
-//   await removeProduct(userToken, cardId);
-//   console.log(`удаление моего товара ${cardId}`);
-// }
+  await removeProduct(userToken, cardId);
+  console.log(`удаление моего товара ${cardId}`);
+}
 
-// async function removeProduct(userToken, _cardId) {
-//   if (requestUserInfo({ token: userToken })) {
-//     await requestRemoveProduct({ token: userToken, _id: _cardId });
-//   }
-// }
+async function removeProduct(userToken, _cardId) {
+  if (requestUserInfo({ token: userToken })) {
+    await requestRemoveProduct({ token: userToken, _id: _cardId });
+  }
+}
 
-// function getId(e) {
-//   console.log(e);
-//   const card = e.path.find(el => el.className === 'card');
-//   return card.dataset.id;
-// }
+function getId(e) {
+  console.log(e);
+  const card = e.path.find(el => el.className === 'card');
+  return card.dataset.id;
+}
