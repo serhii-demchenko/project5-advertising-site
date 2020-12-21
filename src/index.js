ec9985225046a18715d3b33479b15993206ea1ff
Serhii Demchenko <dem.s.vl@gmail.com>
2020-12-21 08:10:28 +0200

    fix google auth

--- src/index.js
+++ src/index.js
@@ -25,18 +25,6 @@ const onLoadPage = async () => {
   recordToCategories(await requestCategories());
   addCategoriesToRouter();
   updatedContent();
-
-  console.log('load page');
-  // login imitation
-  // requestUserLogin({
-  //   email: 'user@example.com',
-  //   password: 'qwerty123',
-  // }).then(obj => {
-  //   console.log(obj);
-  //   sessionStorage.setItem('accessToken', obj.accessToken);
-  //   sessionStorage.setItem('refreshToken', obj.refreshToken);
-  //   sessionStorage.setItem('sid', obj.sid);
-  // });
   markupCategory(categories);
   addListenersInHeader();
   getAddListenersInCard();
@@ -48,38 +36,9 @@ window.onpopstate = async event => {
 };
 addScrollUp();
 setInterval(refreshTokenRequest, 10 * 60 * 1000);
-
-// document.body.insertAdjacentHTML(
-//   'beforeend',
-//   '<input type="file" id="image_uploads" name="image_uploads" accept=".jpg, .jpeg, .png" multiple><button class="button_test">Тык!</button>',
-// );
-// let obj = {};
-
-// const input = document.querySelector('#image_uploads');
-// input.addEventListener('change', e => {
-//   obj = {
-//     title: 'Xiaomi RedmiBook 14 II i7 10th 16/512Gb/MX350 Silver',
-//     description: 'Xiaomi RedmiBook 14 II i7 10th 16/512Gb/MX350 Silver',
-//     category: 'electronics',
-//     price: '25138',
-//     phone: '+380670000000',
-//     file: e.target.files,
-//   };
-// });
-
-// document.querySelector('.button_test').addEventListener('click', e => {
-//   console.log(obj);
-//   requestPostProduct({
-//     token: sessionStorage.getItem('accessToken'),
-//     product: obj,
-//   }).then(console.log);
-// });
-
-// ;
 requestUserById({ userId: '5fd7c4ecc298a200179c89f5' }).then(show);
 
 function show(item) {
   console.log(item);
 }
-
- googleAuth();
+googleAuth();
